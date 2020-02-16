982ba19a5dcd3a10fb96024c853138e6a8b2fc22
Andrzej Ludwikowski <andrzej.ludwikowski@softwaremill.com>
2020-02-16 17:30:06 +0100

    ex3

---
+++ src/main/scala/com/softwaremill/ex3/FileUploaderBehavior.scala
@@ -0,0 +1,51 @@
+package com.softwaremill.ex3
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import com.softwaremill.ex2.FileManager
+
+import scala.util.{Failure, Success}
+
+object FileUploaderBehavior {
+  sealed trait Command
+  case class UploadFile(replyTo: ActorRef[Response]) extends Command
+  case class GetStatus(replyTo: ActorRef[Response])  extends Command
+  case object FinishUploading                        extends Command
+
+  sealed trait Response
+  case object UploadingInProgress extends Response
+  case object FileUploaded        extends Response
+  case object UploadingNotStarted extends Response
+
+  def waitingForStart(fileManager: FileManager): Behavior[Command] = Behaviors.setup { context =>
+    Behaviors.receiveMessage {
+      case UploadFile(replyTo) =>
+        context.pipeToSelf(fileManager.startUpload()) {
+          case Success(_)         => FinishUploading
+          case Failure(exception) => throw exception //some error handling here
+        }
+        uploadingInProgress(replyTo)
+      case GetStatus(replyTo) =>
+        replyTo ! UploadingNotStarted
+        Behaviors.same
+    }
+  }
+
+  def uploadingInProgress(replyTo: ActorRef[Response]): Behavior[Command] = Behaviors.receiveMessage {
+    case FinishUploading =>
+      replyTo ! FileUploaded
+      uploadingFinished()
+    case UploadFile(replyTo) =>
+      replyTo ! UploadingInProgress
+      Behaviors.same
+    case GetStatus(replyTo) =>
+      replyTo ! UploadingInProgress
+      Behaviors.same
+  }
+
+  def uploadingFinished(): Behavior[Command] = Behaviors.receiveMessage {
+    case GetStatus(replyTo) =>
+      replyTo ! FileUploaded
+      Behaviors.same
+  }
+}
---
+++ src/test/scala/com/softwaremill/ex3/FileUploaderBehaviorSpec.scala
@@ -0,0 +1,98 @@
+package com.softwaremill.ex3
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import com.softwaremill.ex2.FileManager
+import com.softwaremill.ex3.FileUploaderBehavior.{GetStatus, Response, UploadFile}
+import org.scalatest.flatspec.AnyFlatSpecLike
+
+class FileUploaderBehaviorSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike {
+  behavior of "FileUploader ex 3"
+
+  private val fileManager = new FileManager
+
+  it should "respond after uploading file" in {
+    // given
+    val uploader = testKit.spawn(FileUploaderBehavior.waitingForStart(fileManager))
+    val probe    = createTestProbe[Response]()
+
+    // when
+    uploader ! UploadFile(probe.ref)
+
+    // then
+    probe.expectMessageType[FileUploaderBehavior.FileUploaded.type]
+  }
+
+  it should "get status while uploading" in {
+    // given
+    val uploader = testKit.spawn(FileUploaderBehavior.waitingForStart(fileManager))
+    val probe    = createTestProbe[Response]()
+    uploader ! UploadFile(probe.ref)
+
+    // when
+    uploader ! GetStatus(probe.ref)
+
+    // then
+    probe.expectMessageType[FileUploaderBehavior.UploadingInProgress.type]
+  }
+
+  it should "get status when not started" in {
+    // given
+    val uploader = testKit.spawn(FileUploaderBehavior.waitingForStart(fileManager))
+    val probe    = createTestProbe[Response]()
+
+    // when
+    uploader ! GetStatus(probe.ref)
+
+    // then
+    probe.expectMessageType[FileUploaderBehavior.UploadingNotStarted.type]
+  }
+
+  it should "not start only one upload at the same time" in {
+    // given
+    val uploader = testKit.spawn(FileUploaderBehavior.waitingForStart(fileManager))
+    val probe    = createTestProbe[Response]()
+
+    // when
+    uploader ! UploadFile(probe.ref)
+    uploader ! UploadFile(probe.ref)
+
+    // then
+    probe.expectMessageType[FileUploaderBehavior.UploadingInProgress.type]
+    probe.expectMessageType[FileUploaderBehavior.FileUploaded.type]
+    probe.expectNoMessage()
+  }
+
+  it should "get status after file is uploaded" in {
+    // given
+    val uploader    = testKit.spawn(FileUploaderBehavior.waitingForStart(fileManager))
+    val probe       = createTestProbe[Response]()
+    val statusProbe = createTestProbe[Response]()
+    uploader ! UploadFile(probe.ref)
+
+    eventually {
+      // when
+      uploader ! GetStatus(statusProbe.ref)
+
+      // then
+      statusProbe.expectMessageType[FileUploaderBehavior.FileUploaded.type]
+    }
+  }
+
+  it should "start next upload when the first is done" in {
+    // given
+    val uploader = testKit.spawn(FileUploaderBehavior.waitingForStart(fileManager))
+    val probe    = createTestProbe[Response]()
+
+    // when
+    uploader ! UploadFile(probe.ref)
+
+    // then
+    probe.expectMessageType[FileUploaderBehavior.FileUploaded.type]
+
+    // when
+    uploader ! UploadFile(probe.ref)
+
+    // then
+    probe.expectMessageType[FileUploaderBehavior.FileUploaded.type]
+  }
+}
