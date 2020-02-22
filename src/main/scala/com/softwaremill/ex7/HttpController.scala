8ce9330221f80e3ab309b74d986ef7e07683f8d4
Andrzej Ludwikowski <andrzej.ludwikowski@softwaremill.com>
2020-02-22 10:44:32 +0100

    ex7 - setup

---
+++ src/main/scala/com/softwaremill/ex7/HttpController.scala
@@ -0,0 +1,36 @@
+package com.softwaremill.ex7
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.receptionist.Receptionist
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.util.Timeout
+import com.softwaremill.ex7.Adder.Result
+import com.softwaremill.ex7.HttpController.{Request, Response}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+class HttpController(system: ActorSystem[Nothing]) {
+  implicit val timeout: Timeout = Timeout(3.seconds)
+  implicit val scheduler        = system.scheduler
+
+  def process(request: Request): Future[Response] = {
+    system.receptionist.ask[Receptionist.Listing](Receptionist.Find(Adder.AdderKey, _)).flatMap {
+      case Adder.AdderKey.Listing(listing) =>
+        listing.headOption match {
+          case Some(adder) => ??? //TODO ask the adder
+          case None        => throw new IllegalStateException("Adder is missing")
+        }
+    }
+  }
+}
+
+object HttpController {
+  case class Request(a: Int, b: Int)
+  case class Response(value: Int)
+
+  object Response {
+    def apply(result: Result): Response = Response(result.value)
+  }
+}
---
+++ src/test/scala/com/softwaremill/ex7/HttpControllerSpec.scala
@@ -0,0 +1,32 @@
+package com.softwaremill.ex7
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import com.softwaremill.ex7.HttpController.{Request, Response}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class HttpControllerSpec extends AnyFlatSpecLike with Matchers with ScalaFutures with BeforeAndAfterAll with Eventually {
+
+  implicit private val system: ActorSystem[Nothing] = ActorSystem(Behaviors.setup[Unit] { ctx =>
+    ctx.spawn(Adder(), "adder")
+    Behaviors.empty
+  }, "test-as")
+  private val httpController = new HttpController(system)
+  behavior of "HttpController"
+
+  it should "calculate result" in {
+    //when then
+    eventually {
+      httpController.process(Request(1, 2)).futureValue shouldBe Response(3)
+      httpController.process(Request(2, 2)).futureValue shouldBe Response(4)
+      httpController.process(Request(3, 2)).futureValue shouldBe Response(5)
+    }
+  }
+
+  override def afterAll(): Unit = {
+    system.terminate()
+  }
+}
