6f4a31ca02b875a063ddbb8b224c84b33f5f6983
Krzysiek Ciesielski <kciesielski@users.noreply.github.com>
2020-02-25 13:45:46 +0100

    ex 5 - solution - self timer

--- src/main/scala/com/softwaremill/ex5/TimerBehavior.scala
+++ src/main/scala/com/softwaremill/ex5/TimerBehavior.scala
@@ -1,20 +1,26 @@
 package com.softwaremill.ex5
 
 import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{Behaviors, TimerScheduler}
+
+import scala.concurrent.duration._
 
 sealed trait Command
 case object PingMsg extends Command
 
 object TimerBehavior {
+  val TimerKey = "scalar-timer"
+  val delay    = 300.millis
 
   val behavior: Behavior[Command] =
-    // TODO
-    Behaviors.setup[Command] { context =>
-      Behaviors.receiveMessage[Command] {
-        case PingMsg =>
-          context.log.info("Self ping received!")
-          Behaviors.same
+    Behaviors.withTimers { timers: TimerScheduler[Command] =>
+      timers.startTimerWithFixedDelay(TimerKey, PingMsg, delay)
+      Behaviors.setup[Command] { context =>
+        Behaviors.receiveMessage[Command] {
+          case PingMsg =>
+            context.log.info("Self ping received!")
+            Behaviors.same
+        }
       }
     }
 }
