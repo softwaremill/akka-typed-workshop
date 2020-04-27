9e1e2db3bf682168a4ca8bc4630d06b5d2fdd5ad
Michał Pawełczyk <michal@localhost.localdomain>
2020-04-27 11:09:35 +0200

    implementation ex2

--- src/main/scala/com/softwaremill/ex2/FileUploaderBehavior.scala
+++ src/main/scala/com/softwaremill/ex2/FileUploaderBehavior.scala
@@ -3,11 +3,15 @@ package com.softwaremill.ex2
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 
-import scala.concurrent.ExecutionContext.Implicits.global
+
 
 object FileUploaderBehavior {
   case class UploadFile(replyTo: ActorRef[FileUploaded])
   case class FileUploaded()
 
-  def uploaderBehavior(fileManager: FileManager): Behavior[UploadFile] = ???
+  def uploaderBehavior(fileManager: FileManager): Behavior[UploadFile] = Behaviors.receiveMessage {
+    case UploadFile(replyTo) =>
+      replyTo ! FileUploaded()
+      Behaviors.same
+  }
 }
