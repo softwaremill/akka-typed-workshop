7cab6bb523c2777da236e91308628f9683eb1684
Sebastian Rysztun <rysztun@gmail.com>
2020-04-27 15:51:39 +0200

    Exercise 6 impl

--- src/main/scala/com/softwaremill/ex6/HttpController.scala
+++ src/main/scala/com/softwaremill/ex6/HttpController.scala
@@ -3,6 +3,7 @@ package com.softwaremill.ex6
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.actor.typed.{ActorRef, ActorSystem, Props, SpawnProtocol}
 import akka.util.Timeout
+import com.softwaremill.ex6.Adder.Result
 import com.softwaremill.ex6.HttpController.{Request, Response}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -17,8 +18,10 @@ class HttpController(system: ActorSystem[SpawnProtocol.Command]) {
     val createAdder: Future[ActorRef[Adder.Add]] =
       system.ask(SpawnProtocol.Spawn(behavior = Adder(), name = "adder", props = Props.empty, _))
 
-    //TODO ask adder
-    ???
+    for {
+      adder <- createAdder
+      res <- adder.ask[Adder.Result](ref => Adder.Add(request.a, request.b, ref))
+    } yield Response(res.value)
   }
 }
 
