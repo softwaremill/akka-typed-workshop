d75328eb12e4d87b5cafc64ac888a99b2ec91166
Michal Swiatowy <mswia@softserveinc.com>
2020-04-27 15:51:38 +0200

    spawn protocol

--- src/main/scala/com/softwaremill/ex6/HttpController.scala
+++ src/main/scala/com/softwaremill/ex6/HttpController.scala
@@ -1,7 +1,7 @@
 package com.softwaremill.ex6
 
+import akka.actor.typed._
 import akka.actor.typed.scaladsl.AskPattern._
-import akka.actor.typed.{ActorRef, ActorSystem, Props, SpawnProtocol}
 import akka.util.Timeout
 import com.softwaremill.ex6.HttpController.{Request, Response}
 
@@ -10,15 +10,17 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 
 class HttpController(system: ActorSystem[SpawnProtocol.Command]) {
-  implicit val timeout: Timeout = Timeout(3.seconds)
-  implicit val scheduler        = system.scheduler
+  implicit val timeout: Timeout     = Timeout(3.seconds)
+  implicit val scheduler: Scheduler = system.scheduler
 
   def process(request: Request): Future[Response] = {
     val createAdder: Future[ActorRef[Adder.Add]] =
       system.ask(SpawnProtocol.Spawn(behavior = Adder(), name = "adder", props = Props.empty, _))
 
-    //TODO ask adder
-    ???
+    for {
+      ref    <- createAdder
+      result <- ref.ask[Adder.Result](Adder.Add(request.a, request.b, _))
+    } yield Response(result.value)
   }
 }
 
