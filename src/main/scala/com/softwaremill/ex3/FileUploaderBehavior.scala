282cf8cf0acc215dff3dca509787ac2459e57a98
Patryk Gała <patryk.kacper.gala@gmail.com>
2020-04-27 11:59:00 +0200

    ex3.2

--- src/main/scala/com/softwaremill/ex3/FileUploaderBehavior.scala
+++ src/main/scala/com/softwaremill/ex3/FileUploaderBehavior.scala
@@ -1,40 +1,45 @@
 package com.softwaremill.ex3
 
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
 import com.softwaremill.ex2.FileManager
 
 import scala.util.{Failure, Success}
 
 object FileUploaderBehavior {
+
   sealed trait Command
+
   case class UploadFile(replyTo: ActorRef[Response]) extends Command
-  case class GetStatus(replyTo: ActorRef[Response])  extends Command
-  case object FinishUploading                        extends Command
+
+  case class GetStatus(replyTo: ActorRef[Response]) extends Command
+
+  private case object FinishUploading extends Command
 
   sealed trait Response
+
   case object UploadingInProgress extends Response
-  case object FileUploaded        extends Response
+
+  case object FileUploaded extends Response
+
   case object UploadingNotStarted extends Response
 
   def waitingForStart(fileManager: FileManager): Behavior[Command] = Behaviors.setup { context =>
-    Behaviors.receiveMessage {
+    Behaviors.receiveMessagePartial {
       case UploadFile(replyTo) =>
-        context.pipeToSelf(fileManager.startUpload()) {
-          case Success(_)         => FinishUploading
-          case Failure(exception) => throw exception //some error handling here
-        }
-        uploadingInProgress(replyTo)
+        startUpload(fileManager, context)
+        uploadingInProgress(replyTo, fileManager)
       case GetStatus(replyTo) =>
         replyTo ! UploadingNotStarted
         Behaviors.same
     }
   }
 
-  def uploadingInProgress(replyTo: ActorRef[Response]): Behavior[Command] = Behaviors.receiveMessage {
+
+  private def uploadingInProgress(replyTo: ActorRef[Response], fileManager: FileManager): Behavior[Command] = Behaviors.receiveMessage {
     case FinishUploading =>
       replyTo ! FileUploaded
-      uploadingFinished()
+      uploadingFinished(fileManager)
     case UploadFile(replyTo) =>
       replyTo ! UploadingInProgress
       Behaviors.same
@@ -43,9 +48,23 @@ object FileUploaderBehavior {
       Behaviors.same
   }
 
-  def uploadingFinished(): Behavior[Command] = Behaviors.receiveMessage {
-    case GetStatus(replyTo) =>
-      replyTo ! FileUploaded
-      Behaviors.same
+  private def uploadingFinished(fileManager: FileManager): Behavior[Command] =
+    Behaviors.setup { context =>
+      Behaviors.receiveMessagePartial {
+        case UploadFile(replyTo) =>
+          startUpload(fileManager, context)
+          uploadingInProgress(replyTo, fileManager)
+        case GetStatus(replyTo) =>
+          replyTo ! FileUploaded
+          Behaviors.same
+      }
+    }
+
+
+  private def startUpload(fileManager: FileManager, context: ActorContext[Command]) = {
+    context.pipeToSelf(fileManager.startUpload()) {
+      case Success(_) => FinishUploading
+      case Failure(exception) => throw exception //some error handling here
+    }
   }
 }
