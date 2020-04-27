60c35ec122b929e0ae80c593e1645be57267508c
Slawek Kruczek <slawomir.kruczek@workday.com>
2020-04-27 09:40:21 +0100

    zadanie 1

--- src/main/scala/com/softwaremill/ex1/SquareRootCalculator.scala
+++ src/main/scala/com/softwaremill/ex1/SquareRootCalculator.scala
@@ -1,11 +1,24 @@
 package com.softwaremill.ex1
 
-import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
 
 object SquareRootCalculator {
-  def apply(): Behavior[Calculate] = ???
 
-  case class Calculate() //TODO add required fields
+  def apply(): Behavior[Calculate] = Behaviors.setup { context =>
+    Behaviors.receiveMessage {
+      case Calculate(x, replyTo) if x < 0 =>
+        context.log.warn("Negative integer!")
+        replyTo ! SqrtResult.Error(s"$x is negative, need more powerful math")
+        Behaviors.same
+
+      case Calculate(x, replyTo) =>
+        replyTo ! SqrtResult.Result(math.sqrt(x))
+        Behaviors.same
+    }
+  }
+
+  case class Calculate(x: Int, replyTo: ActorRef[SqrtResult])
 
   sealed trait SqrtResult
 
