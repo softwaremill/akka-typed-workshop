089c8ca254fab52e078e4ec76cdb5927e247776f
Marcin Burczak <marcin.burczak@gmail.com>
2020-04-27 10:39:25 +0200

    Ex1

--- src/main/scala/com/softwaremill/ex1/SquareRootCalculator.scala
+++ src/main/scala/com/softwaremill/ex1/SquareRootCalculator.scala
@@ -1,11 +1,29 @@
 package com.softwaremill.ex1
 
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorRef, Behavior, LogOptions}
+import akka.actor.typed.scaladsl.Behaviors
+import com.softwaremill.ex1.SquareRootCalculator.SqrtResult.{Error, Result}
+import org.slf4j.event.Level
 
 object SquareRootCalculator {
-  def apply(): Behavior[Calculate] = ???
 
-  case class Calculate() //TODO add required fields
+  def withLog(): Behavior[Calculate] = Behaviors.logMessages(LogOptions().withLevel(Level.WARN), apply())
+
+  def apply(): Behavior[Calculate] = Behaviors.setup { context =>
+    Behaviors.receiveMessage {
+      case Calculate(number, replyTo) =>
+        if (number < 0) {
+          context.log.warn("Negative integer!")
+          replyTo ! Error("Number " + number + " is less than 0")
+        } else {
+          replyTo ! Result(Math.sqrt(number))
+
+        }
+        Behaviors.same
+    }
+  }
+
+  case class Calculate(number: Int, replyTo: ActorRef[SqrtResult])
 
   sealed trait SqrtResult
 
